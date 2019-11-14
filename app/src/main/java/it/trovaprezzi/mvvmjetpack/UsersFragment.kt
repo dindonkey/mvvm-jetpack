120f7bb4e19fd2cc79f2567aed0235f036ac39d9
dindonkey <simone.caldon@gmail.com>
2019-11-14 13:52:46 +0100

    data binding

--- app/src/main/java/it/trovaprezzi/mvvmjetpack/UsersFragment.kt
+++ app/src/main/java/it/trovaprezzi/mvvmjetpack/UsersFragment.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import it.trovaprezzi.mvvmjetpack.databinding.UsersFragmentBinding
 
-class UsersFragment: Fragment() {
+class UsersFragment : Fragment() {
+    private lateinit var usersFragmentBinding: UsersFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        usersFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.users_fragment,
+            container,
+            false
+        )
 
-        return inflater.inflate(R.layout.users_fragment, container, false)
+        return usersFragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        usersFragmentBinding.user = User("pippo")
     }
 }
